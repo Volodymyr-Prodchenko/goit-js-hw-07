a0f220268a10662d9a7c8320c215f83769d734af
Volodymyr Prodchenko <vladimir.prodchenko@gmail.com>
2024-05-20 23:52:32 +0300

    add hw-js-07-task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,30 +1,42 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
 const input = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
 
-createBtn.addEventListener('click', () => createBoxes(input.value));
-destroyBtn.addEventListener('click', destroyBoxes);
+function createBoxes(amount) {
+  let size = 30;
+  const boxes = [];
+  
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  } 
+  
+  boxesContainer.append(...boxes);
+}
 
 function destroyBoxes() {
   boxesContainer.innerHTML = '';
 }
-function createBoxes(amount) {
-  destroyBoxes();
-  let size = 30;
-  let boxContent = '';
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value);
   if (amount >= 1 && amount <= 100) {
-    for (let i = 0; i < amount; i++) {
-      boxContent += `<div style ="background:${getRandomHexColor()}; width: ${size}px;height:${size}px;"></div>`;
-      size += 10;
-    }
-  } else alert('Select number between 1-100');
-  boxesContainer.innerHTML = boxContent;
-  input.value = '';
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
